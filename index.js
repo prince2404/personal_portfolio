b116ebfeb32c81ec3019a25e09799575a45e0a03
prince2404 <166131044+prince2404@users.noreply.github.com>
2024-09-17 21:00:25 -0700

    contact

--- index.js
+++ index.js
@@ -28,3 +28,24 @@ function closemenu(){
 }
 
 
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzg2o5wOGt-TSzxD2D93G9292xzunZLLzeyJwam7YhV80yvoRUxejNXh2JmEKVkUYba/exec'
+const form = document.forms['submit-to-google-sheet']
+
+const msg = document.getElementById("msg")
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        msg.innerHTML = "Message sent successfully"
+        setTimeout(function(){
+            msg.innerHTML = ""
+        },3000)
+        form.reset()
+      })
+      .catch(error => console.error('Error!', error.message))
+});
+
+
+
